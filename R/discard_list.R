c5ac993e9e5bb5226671a1730829bf97d2edcaa5
andr3wli <andrewlll9@hotmail.ca>
2022-02-07 14:52:51 -0800

    First function

---
+++ R/discard_list.R
@@ -0,0 +1,23 @@
+#' Create a list with participant numbers that are both in the batch data set and the mturk data set
+#'
+#' @param df1 the batch data set provided by amazon
+#' @param df2 the mturk data set that we created locally
+#'
+#' @return a character vector
+#' @export
+#'
+#' @examples
+#' discard_list(batch_data, mturk_data)
+discard_list <- function(df1, df2) {
+  keep <- vector("character", nrow(batch_data))
+
+  for (i in df1$Answer.surveycode) {
+    for (j in df2$Code) {
+      ps <- i
+      if(i %in% j) {
+        keep[[i]] <- ps
+      }
+    }
+  }
+  return(keep)
+}
