acbc7f13bb5557787206d27821fc4ee17bf84e2e
andr3wli <andrewlll9@hotmail.ca>
2022-02-07 15:09:49 -0800

    Created failed_attention_check() function and added dependencies

---
+++ R/failed_attention_check.R
@@ -0,0 +1,18 @@
+#' Create a vector with all the participants that failed the attention check
+#'
+#' @param df data frame with the attention check question
+#' @param question the attention check question as a string
+#'
+#' @return a vector of all the participants that failed the attention check
+#' @export
+#'
+#' @examples
+#' attention_question <- "This is an attention check"
+#' failed_attention_check <- (petition_data, attention_question)
+failed_attention_check <- function(df, question) {
+
+  failed_vector <- df |>
+    dplyr::filter(Question == attention_question & Rawresponse !=5) |>
+    dplyr::pull(MTurk)
+  return(failed_vector)
+}
