0398be04b53a0422292c259a0a9cf501e659abac
andr3wli <andrewlll9@hotmail.ca>
2022-02-08 11:37:26 -0800

    Wrote a test for the create_reject_col function

--- tests/testthat/test-create_reject_col.R
+++ tests/testthat/test-create_reject_col.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# create the keep and the failed attention check vectors needed for this function
+keep <- discard_list(df1 = batch, df2 = mturk)
+attention_question <- "This is an attention check. Please select ‘strongly agree’ to pass this check. You will not be compensated if you fail this check."
+failed <- failed_attention_check(df = analysis, question = attention_question)
+
+# create the approve column
+clean_batch <- create_approve_col(batch, keep = keep, fail = failed)
+
+# create the reject column
+clean_reject_batch <- create_reject_col(clean_batch, keep = keep, fail = failed)
+
+# tests
+test_that("the approve column exists", {
+  expect_true("Reject" %in% colnames(clean_batch))
+})
+
+test_that("The Reject column contains the correct contents", {
+  expect_true("MTurk code does not match." %in% clean_reject_batch$Reject)
+  expect_true("" %in% clean_reject_batch$Reject)
 })
